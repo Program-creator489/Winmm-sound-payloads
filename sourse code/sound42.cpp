99fe303bf9c9d9e19707b9f8db51597718462d5f
Program-creator489 <kobralu21@gmail.com>
2026-09-13 19:52:04 -0500

    Add files via upload

---
+++ sourse code/sound42.cpp
@@ -0,0 +1,22 @@
+#include <windows.h>
+#pragma comment(lib, "winmm.lib")
+
+VOID WINAPI sound1() {
+    HWAVEOUT hWaveOut = 0;
+    WAVEFORMATEX wfx = { WAVE_FORMAT_PCM, 1, 32000, 32000, 1, 8, 0 };
+    waveOutOpen(&hWaveOut, WAVE_MAPPER, &wfx, 0, 0, CALLBACK_NULL);
+    char buffer[32000 * 30] = {};
+    for (DWORD t = 0; t < sizeof(buffer); ++t)
+        buffer[t] = static_cast<char>((t>>6|t<<1)+(t>>5|t<<3|t>>3)|t>>2|t>>1|t>>5|t>>9);
+
+    WAVEHDR header = { buffer, sizeof(buffer), 0, 0, 0, 0, 0, 0 };
+    waveOutPrepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
+    waveOutWrite(hWaveOut, &header, sizeof(WAVEHDR));
+    waveOutUnprepareHeader(hWaveOut, &header, sizeof(WAVEHDR));
+    waveOutClose(hWaveOut);
+}
+int main()
+{
+            sound1();
+            Sleep(30000);
+ }
